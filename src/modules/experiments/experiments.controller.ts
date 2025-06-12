19a833606d3b9ed147918a910cbc4a9c3374aa2a
Alexander Rubin <alexandrrubin0715@gmail.com>
2025-06-12 03:28:10 +0300

    fix import

--- src/modules/experiments/experiments.controller.ts
+++ src/modules/experiments/experiments.controller.ts
@@ -11,7 +11,7 @@ import { ExperimentsService } from './experiments.service';
 import { CreateExperimentDto } from './dtos/input/create-experiment.dto';
 import { CompleteExperimentParamDto } from './dtos/input/complete-experiment-params.dto';
 import { GetActiveExperimentsForDeviceAndAssignToNewParamDto } from './dtos/input/get-active-experiments-for-device-and-assign-to-new-params.dto';
-import { BasicAuthGuard } from 'src/common/guards/auth.guard';
+import { BasicAuthGuard } from '../../common/guards/auth.guard';
 
 @Controller('experiments')
 export class ExperimentsController {
